467fc70d8ecc59d07098f8e176faa4cd3a99e4ea
Kirk Roerig <kirk.roerig@modustri.com>
2016-04-13 22:23:02 -0400

    Fixed allocation bug, fixed string writing, added optional 0 pad to strings

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ Object.prototype.structify = function(lo){
 	var isBE = lo.isBigEndian;
 	var buf = new Buffer(16);
 	var off = 0;
+
+	buf.fill(0);
 	for(var i = 0; i < lo.length; ++i){
 		var name = lo[i].name;
 		var type = lo[i].type;
@@ -16,16 +18,15 @@ Object.prototype.structify = function(lo){
 		function grow(newBytes){
 			if(off + newBytes >= buf.length){
 				var newBuf = new Buffer(buf.length << 1);
+				newBuf.fill(0);
 				buf.copy(newBuf);
 				buf = newBuf;
 			}
 		}
 
-		console.log(name);
-
 		switch(type[0]){
-		case 'f': call += 'Float';  break;
-		case 'd': call += 'Double'; break;
+		case 'f': call += 'Float'; bpe = 4;  break;
+		case 'd': call += 'Double'; bpe = 8; break;
 		case 'u': call += 'UInt'; isInt = true; break;
 		case 'i': call += 'Int'; isInt = true; break;
 		}
@@ -41,9 +42,11 @@ Object.prototype.structify = function(lo){
 		}
 
 		if(lo[i].size && bpe == 1){
-			grow(lo[i].size);
-			buf.write(this[name], off, lo[i].size, 'ascii');
-			off += lo[i].size;
+			var size = lo[i].size;
+			var str = this[name];
+			grow(size);
+			buf.write(str, off, size > str.len ? str.len : size, 'ascii');
+			off += size;
 		}
 		else{
 			grow(bpe);
